d984269211cebfb26d600b10f629e8ba6b4379ba
tlaufkoetter <5479245+tlaufkoetter@users.noreply.github.com>
2024-12-14 20:17:35 +0100

    Day 7 - no branching

--- day-seven/DaySeven/Program.cs
+++ day-seven/DaySeven/Program.cs
@@ -1,20 +1,52 @@
-﻿
-Console.WriteLine(File.ReadAllLines("input.txt")
-    .Select(line => line.Split(':')
-        .SelectMany(part => part.Trim().Split(' '))
-        .Where(part => !string.IsNullOrWhiteSpace(part))
-        .Select(long.Parse))
-    .Select(parts => new Equation(parts.First(), parts.Skip(1).ToArray()))
-    .Where(HasSolution)
-    .Sum(equation => equation.Result));
+﻿var lines = File.ReadAllLines("input.txt");
+var equations = lines
+    .Select(ParseEquation)
+    .Where(HasSolution);
+var solution = AggregateSolutions(equations);
+Console.WriteLine($"Found solution: {solution}");
+Console.WriteLine($"{new long[] { 663613490587 }.Contains(solution)}");
 
+Equation ParseEquation(string line)
+{
+    var parts = line.Split(':')
+            .SelectMany(part => part.Trim().Split(' '))
+            .Where(part => !string.IsNullOrWhiteSpace(part))
+            .Select(long.Parse);
+    return new Equation(parts.First(), parts.Skip(1).ToArray());
+}
+
+long AggregateSolutions(IEnumerable<Equation> equations)
+{
+    return equations.Where(HasSolution).Sum(equation => equation.Result);
+}
+
+Equation Advance(Equation equation, Func<long, long, long> operand)
+{
+    return equation with { Result = operand(equation.Result, equation.Coefficients.First()), Coefficients = equation.Coefficients.Skip(1).ToArray() };
+}
+
+long Add(long a, long b)
+{
+    return a + b;
+}
+
+long Mult(long a, long b)
+{
+    return a * b;
+}
+
+
+IEnumerable<long> CalculateSolutions(Equation equation)
+{
+    Func<long, long, long>[] operands = [Add, Mult];
+    var firsts = equation.Coefficients.Take(1).ToArray();
+    return new long[] { equation.Result }.Take(Math.Abs(firsts.Length - 1))
+        .Concat(firsts.SelectMany(_ => operands.SelectMany(operand => CalculateSolutions(Advance(equation, operand)))));
+}
 
 bool HasSolution(Equation equation)
 {
-    return equation.Result == 0 && equation.Coefficients.Length == 0
-    || equation.Coefficients.Length != 0 && equation.Result > 0
-        && (HasSolution(equation with { Result = equation.Result - equation.Coefficients.Last(), Coefficients = equation.Coefficients.SkipLast(1).ToArray() })
-            || equation.Result % equation.Coefficients.Last() == 0 && HasSolution(equation with { Result = equation.Result / equation.Coefficients.Last(), Coefficients = equation.Coefficients.SkipLast(1).ToArray() }));
+    return CalculateSolutions(Advance(equation with { Result = 0 }, Add)).Any(solution => solution == equation.Result);
 }
 record Equation(long Result, long[] Coefficients);
 
