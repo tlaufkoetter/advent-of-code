c391fbc7438e66d38372cf8deda7012993e41160
tlaufkoetter <5479245+tlaufkoetter@users.noreply.github.com>
2024-12-14 20:17:36 +0100

    Day Three - part 2

--- TwentyTwentyFour/DayThree.cs
+++ TwentyTwentyFour/DayThree.cs
@@ -15,11 +15,40 @@ public class DayThree
         Assert.Equal(165225049, GetMultipliedSum());
     }
 
-    private static long GetMultipliedSum()
+    [Fact]
+    void Part2()
+    {
+        Assert.Equal(108830766, GetEnabledMultipliedSum());
+    }
+
+    private static Dictionary<int, long> GetMuls(string text)
     {
         return Regex.Matches(GetText(), @"mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)")
                     .Where(match => match.Success)
-                    .Select(match => long.Parse(match.Groups["first"].Value) * long.Parse(match.Groups["second"].Value))
-                    .Sum();
+                    .ToDictionary(match => match.Index, match => long.Parse(match.Groups["first"].Value) * long.Parse(match.Groups["second"].Value));
+    }
+
+    private static Dictionary<int, bool> GetDos(string text)
+    {
+        return Regex.Matches(GetText(), @"do(n't)?\(\)")
+                    .Where(match => match.Success)
+                    .ToDictionary(match => match.Index, match => match.Value.Equals("do()"));
+    }
+
+    private static long GetMultipliedSum()
+    {
+        return GetMuls(GetText()).Values.Sum();
+    }
+
+    private static long GetEnabledMultipliedSum()
+    {
+        var text = GetText();
+        var dos = GetDos(text);
+        var muls = GetMuls(text);
+        return dos.Keys.Concat(muls.Keys).Order().Aggregate((0L, true), (agg, i) =>
+        {
+            var doo = dos.Where(kvp => kvp.Key.Equals(i)).Select(kvp => kvp.Value).Append(agg.Item2).First();
+            return (agg.Item1 + muls.Where(kvp => kvp.Key.Equals(i)).Sum(kvp => kvp.Value) * doo.CompareTo(false), doo);
+        }).Item1;
     }
 }
