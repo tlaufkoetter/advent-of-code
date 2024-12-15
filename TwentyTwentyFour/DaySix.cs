81370265ebf20f7712ada58d284c7893247ec2f5
tlaufkoetter <5479245+tlaufkoetter@users.noreply.github.com>
2024-12-15 13:29:17 +0100

    Day 6 - Part 1 and 2

---
+++ TwentyTwentyFour/DaySix.cs
@@ -0,0 +1,99 @@
+namespace TwentyTwentyFour;
+
+public static class DaySixExtensions
+{
+    public static IEnumerable<TResult> AsRange<TResult>(this Func<TResult, TResult> source, TResult seed, int count)
+    {
+        return Enumerable.Repeat(false, count).Select(_ =>
+        {
+            seed = source(seed);
+            return seed;
+        });
+    }
+
+    public static IEnumerable<TSource> TakeWhileIncluding<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+    {
+        var enumerator = source.GetEnumerator();
+        var whytho = Enumerable.Repeat(false, int.MaxValue)
+            .TakeWhile(_ => enumerator.MoveNext())
+            .Select(_ => enumerator.Current)
+            .TakeWhile(predicate);
+
+        return whytho.Concat(Enumerable.Repeat(false, 1)
+            .TakeWhile(_ => enumerator.MoveNext())
+            .Select(_ => enumerator.Current));
+    }
+}
+
+public class DaySix
+{
+    public record InputData(Guard Guard, HashSet<Point> Obstacles, Point MapDimensions);
+    public record Guard(Point Position, Point Direction) : IComparable<Guard>
+    {
+        public int CompareTo(Guard? other)
+        {
+            return new int[] { Position.CompareTo(other!.Position), Direction.CompareTo(other.Direction) }.MaxBy(Math.Abs);
+        }
+
+        public Guard Move()
+        {
+            return this with { Position = Position + Direction };
+        }
+
+        public Guard Rotate()
+        {
+            return this with { Direction = new Point(-Direction.Y, Direction.X) };
+        }
+    }
+
+    private static InputData GetInput(string file)
+    {
+        var dict = File.ReadAllLines(file)
+            .SelectMany((row, y) => row.Select((cell, x) => (cell, new Point(x, y))))
+            .GroupBy(cell => cell.cell, cell => cell.Item2)
+            .ToDictionary(group => group.Key, group => group.AsEnumerable());
+        return new InputData(new Guard(dict['^'].First(), new Point(0, -1)), dict['#'].ToHashSet(), dict.Values.SelectMany(p => p).Max());
+    }
+
+    [Theory]
+    [InlineData(["../../../DaySixExample.txt", 41])]
+    [InlineData(["../../../DaySixInput.txt", 5080])]
+    public void Part1(string inputFile, int expected)
+    {
+        Assert.Equal(expected, GetPositions(GetInput(inputFile)).Select(point => point.Position).Distinct().Count());
+    }
+
+    [Theory]
+    [InlineData(["../../../DaySixExample.txt", 6])]
+    [InlineData(["../../../DaySixInput.txt", 1919])]
+    public void Part2(string inputFile, int expected)
+    {
+        var input = GetInput(inputFile);
+        var originalPath = GetPositions(input).Where(pos => !input.Guard.Position.Equals(pos.Position)).Distinct().ToList();
+        var circularPaths = originalPath
+            .Select(guard => guard.Position)
+            .Select((extraObstacle, i) => (extraObstacle, i))
+            .DistinctBy(p => p.extraObstacle)
+            .Select((pair, i) => GetPositions(input with { Obstacles = [.. input.Obstacles, pair.extraObstacle] })
+                .ToList())
+            .Where(path => path.Count > path.Distinct().Count())
+            .Select(p => p.ToHashSet())
+            .Distinct(HashSet<Guard>.CreateSetComparer());
+        Assert.Equal(expected, circularPaths.Count());
+    }
+
+    private static IEnumerable<Guard> GetPositions(InputData input)
+    {
+        HashSet<Guard> visited = [];
+        var guard = input.Guard;
+        var mapDimensions = input.MapDimensions;
+        return new Func<Guard, Guard>(currentGuard =>
+                new Guard[]  {
+                    currentGuard.Move(),
+                    currentGuard.Rotate(),
+                }[Convert.ToInt16(input.Obstacles.Contains(currentGuard.Move().Position))])
+            .AsRange(guard, (int)(input.MapDimensions.X * input.MapDimensions.Y))
+            .TakeWhile(guard => Math.Min(1, Math.Max(0, 1 + Math.Min(guard.Position.CompareTo(new Point(-1, -1)), mapDimensions.CompareTo(guard.Position)))) == 1)
+            .TakeWhileIncluding(visited.Add);
+    }
+}
--- TwentyTwentyFour/Point.cs
+++ TwentyTwentyFour/Point.cs
@@ -1,5 +1,5 @@
 namespace TwentyTwentyFour;
-public readonly struct Point(long x, long y)
+public readonly struct Point(long x, long y) : IComparable<Point>
 {
     public long X { get; } = x;
     public long Y { get; } = y;
@@ -27,5 +27,15 @@ public readonly struct Point(long x, long y)
     public static Point operator %(Point a, Point b)
     {
         return new Point(a.X % b.X, a.Y % b.Y);
+    }
+
+    public override string ToString()
+    {
+        return $"({X},{Y})";
+    }
+
+    public int CompareTo(Point other)
+    {
+        return new int[] { X.CompareTo(other.X), Y.CompareTo(other.Y) }.Where(c => c != 0).Order().FirstOrDefault(0);
     }
 }
